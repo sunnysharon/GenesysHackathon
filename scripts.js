1d16f59e6dc301b77bb150a76e4ef40fce06622b
sunnysharon <chenxuan.wan@mail.utoronto.ca>
2017-11-12 12:12:35 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,6 +18,9 @@ options = {
 
 var button = Dropbox.createChooseButton(options);
 document.getElementById("dropbox-container").appendChild(button);
+const platformClient = require('platformClient');
+var client;
+var destURL;
 
 function add_img_to_list(file) {
   var li  = document.createElement('li');
@@ -41,25 +44,23 @@ function add_link_to_list(file) {
 
 
 function authenticate(file){
-	const platformClient = require('platformClient');
-	
-	var client = platformClient.ApiClient.instance;
+	client = platformClient.ApiClient.instance;
 	client.loginImplicitGrant('3f3a804c-a0de-4f3d-bff7-41a4de3cecff', 'https://localhost:3000')
 	  .then(function() {
+		console.log('authenticated', arguments);
 	    // Do authenticated things
 	    do_post(file);
 
 	  })
 	  .catch(function(response) {
-	  	console.log("ERR LEVEL 1")
+		console.log("ERR LEVEL 1")
 	    //console.log(`${response.status} - ${response.error.message}`);
 	    console.log(response.error);
 	  });
-
 }
 
 function do_post(file){
-		// Browser
+	
 	platformClient = require('platformClient');
 	var apiInstance = new platformClient.ContentManagementApi();
 	console.log("got to");
@@ -74,10 +75,42 @@ function do_post(file){
 	apiInstance.postContentmanagementDocuments(body, opts)
 	  .then(function(data) {
 	    console.log(`postContentmanagementDocuments success! data: ${JSON.stringify(data, null, 2)}`);
+		destURL = data.uploadDestinationUri
+		do_post_helper(file);
 	  })
 	  .catch(function(error) {
-	  	console.log("ERR LEVEL 2")
-	  	console.log('There was a failure calling postContentmanagementDocuments');
+		console.log("ERR LEVEL 2")
+		console.log('There was a failure calling postContentmanagementDocuments');
 	    console.error(error);
 	  });
 }
+
+
+function do_post_helper(file){
+
+
+	console.log(destURL)
+	var token = "Bearer "+client.authentications["PureCloud Auth"].accessToken
+	console.log("token")
+
+	console.log(token)
+
+
+    var body = {
+	"name": "somename",
+	"filename": "ExampleUpload.txt",
+	"file contents": "This is an example file body with one line of text for upload.",
+	"Content-Type": "text/plain"
+	};
+
+
+
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", destURL, true);
+	xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=A4Q9L5049cRQj9hx8FK8I7bep3YXSsN0hTNa");
+	xhr.setRequestHeader("Authorization", token);
+	xhr.send(body);
+
+}
+
